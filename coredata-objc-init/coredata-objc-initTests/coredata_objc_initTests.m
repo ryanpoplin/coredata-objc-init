ff4db192aba95e31c8bca8a6c9d01410f481d1e6
Ryan Poplin <ryanpoplin@Ryans-Mac-mini.local>
2015-12-21 13:24:03 -0500

    init setup

---
+++ coredata-objc-init/coredata-objc-initTests/coredata_objc_initTests.m
@@ -0,0 +1,39 @@
+//
+//  coredata_objc_initTests.m
+//  coredata-objc-initTests
+//
+//  Created by Ryan Poplin on 12/21/15.
+//  Copyright © 2015 Ryan Poplin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface coredata_objc_initTests : XCTestCase
+
+@end
+
+@implementation coredata_objc_initTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
