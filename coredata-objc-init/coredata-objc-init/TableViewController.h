8090025329c01f5c595c9e29750f8d1638fac722
Ryan Poplin <ryanpoplin@Ryans-Mac-mini.local>
2015-12-22 16:05:56 -0500

    coredata init

---
+++ coredata-objc-init/coredata-objc-init/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  coredata-objc-init
+//
+//  Created by Ryan Poplin on 12/21/15.
+//  Copyright © 2015 Ryan Poplin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
